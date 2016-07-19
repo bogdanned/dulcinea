112cdbc4e52bae39ea5bc1b01d8e32ccdf7145ff
Raúl Berganza <brgraul@gmail.com>
2016-07-19 19:37:01 +0200

    build darkroom

--- dulcinea/urls.py
+++ dulcinea/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rocinante/', include('app_rocinante.urls')),
+    url(r'^quijote/', include('app_quijote.urls')),
 ]
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Brg51501/Desktop/Dulcinea/dulcinea/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
