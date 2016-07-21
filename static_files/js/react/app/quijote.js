924eb0dc5d2662e8476a7945bdb62d2c9a1a26df
Dragos <dragosgn@gmail.com>
2016-07-21 11:25:00 +0200

    dragos commit

---
+++ static_files/js/react/app/quijote.js
@@ -0,0 +1,50 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var forms = require('newforms')
+var BootstrapForm = require('newforms-bootstrap')
+var Tether = require('react-tether');
+
+var myDataSource = {
+    chart: {
+        caption: "Harry's SuperMart",
+        subCaption: "Top 5 stores in last month by revenue",
+        numberPrefix: "$",
+    },
+    data:[
+        {
+            label: "Bakersfield Central",
+            value: "880000"
+        },
+        {
+            label: "Garden Groove harbour",
+            value: "730000"
+        },
+        {
+            label: "Los Angeles Topanga",
+            value: "590000"
+        },
+        {
+            label: "Compton-Rancho Dom",
+            value: "520000"
+        },
+        {
+            label: "Daly City Serramonte",
+            value: "330000"
+        }
+    ]
+};
+
+var chartConfigs = {
+    id: "revenue-chart",
+    renderAt: "revenue-chart-container",
+    type: "column2d",
+    width:600,
+    height: 400,
+    dataFormat: "json",
+    dataSource: myDataSource
+};
+
+React.render(
+    <react_fc.FusionCharts {...chartConfigs} />,
+    document.getElementById("chart-container")
+);
